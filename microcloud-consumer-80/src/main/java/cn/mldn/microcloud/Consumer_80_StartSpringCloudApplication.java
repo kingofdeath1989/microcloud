e6ea59a62b45ae805480f3c0b6006063bd7bff23
liyue <liyue@qq.com>
2019-04-24 18:56:03 +0800

    microcloud-consumer-80

---
+++ microcloud-consumer-80/src/main/java/cn/mldn/microcloud/Consumer_80_StartSpringCloudApplication.java
@@ -0,0 +1,17 @@
+package cn.mldn.microcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
+import cn.mldn.commons.config.MyLoadBalanceConfig;
+@SpringBootApplication
+@EnableEurekaClient
+@RibbonClient(name="ribbonClient",configuration=MyLoadBalanceConfig.class)
+public class Consumer_80_StartSpringCloudApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(Consumer_80_StartSpringCloudApplication.class,
+				args);
+	}
+}
